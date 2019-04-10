5d4efca719c694b429fa5e986a81e9de33aee888
Olegus <gulldan1996@gmail.com>
2019-04-10 14:58:42 +0300

    arrayMethodMap

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -4,8 +4,17 @@
  * Implement method Map
  */
 function applyCustomMap() {
-  [].__proto__.map2 = function() {
-    // write code here
+  [].__proto__.map2 = function(callback) {
+    const items = this;
+    const resultItems = [];
+
+    for (let i = 0; i < items.length; i++) {
+      let item = items[i];
+      let newItem = callback(item, i, items);
+
+      resultItems[i] = newItem;
+    }
+    return resultItems;
   };
 }
 
