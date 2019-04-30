1f649930d867dc23e011e9ebce4879e69e38e80b
Onisko <Onisko@MacBook.local>
2019-04-30 14:16:48 +0300

    sol

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -6,6 +6,13 @@
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
     // write code here
+    let arr = [];
+    console.log(this);
+    this.forEach(function(el, i, thisArr) {
+      arr.push(callback(el, i, thisArr));
+    });
+    console.log(arr);
+    return arr;
   };
 }
 
