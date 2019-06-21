f7956c046fd3a6964a44ca22171038702f13bf44
scandal0us <d.l.lutsak@gmail.com>
2019-06-21 19:42:34 +0300

    Soltion

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -6,6 +6,12 @@
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
     // write code here
+    const result = [];
+    const length = this.length;
+    for (let i = 0; i < length; i++) {
+      result[result.length] = callback(this[i], i, this);
+    }
+    return result;
   };
 }
 
