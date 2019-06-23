9ad1fd4458ce67bfbbb92670f67917d740d8cf5e
Taisiia <taisiia.pomazan@gmail.com>
2019-06-23 17:25:27 +0300

    sol

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,8 +5,14 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      result[result.length] = callback(this[i], i, this);
+    };
+
+    return result;
   };
-}
+};
 
 module.exports = applyCustomMap;
