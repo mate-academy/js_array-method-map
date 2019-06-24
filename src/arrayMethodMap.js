06546d29824abf650a0b18287b886c17ca224115
DimaKashyrin <kashyrindima@gmail.com>
2019-06-24 03:26:41 +0300

    solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,15 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const resultArr = [];
+    const arr = this;
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+      const index = i;
+
+      resultArr[index] = callback(item, index, arr);
+    }
+    return resultArr;
   };
 }
 
