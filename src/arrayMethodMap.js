6bc808c380c3680aceb96ecca4793ecb1f8a71f7
igaryok <igor.sobko@gmail.com>
2019-08-19 15:25:25 +0300

    solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,13 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const resultsArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+      resultsArray.push(callback(this[i], i, this));
+    }
+
+    return resultsArray;
   };
 }
 
