10a1c24282455951a31493e4e7fdb1320d9032e3
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-08-20 08:12:42 +0300

    Solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,11 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const resultMap2 = [];
+    for (let i = 0; i < this.length; i++) {
+      resultMap2[i] = callback(this[i], i, this);
+    }
+    return resultMap2;
   };
 }
 
