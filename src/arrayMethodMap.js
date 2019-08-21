c44d0a981324fd472b223dfd34528efecd0d3345
vladyslav223 <vladyslav223@gmail.com>
2019-08-21 21:10:25 +0300

    updated map2

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -1,17 +1,13 @@
 'use strict';
 
-/**
- * Implement method Map
- */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
-    const newArr = [];
+    const mapped = [];
     for (let i = 0; i < this.length; i++) {
-      newArr[newArr.length] = this[i];
-      newArr[newArr.length - 1] = callback(this[i], i, this);
+      mapped[i] = callback(this[i], i, this);
     }
-    return newArr;
+
+    return mapped;
   };
 };
 
