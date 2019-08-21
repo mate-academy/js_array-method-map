889fd03575ff2366774665c04d30509ea24bcfab
vladyslav223 <vladyslav223@gmail.com>
2019-08-21 08:37:11 +0300

    test

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -6,7 +6,13 @@
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
     // write code here
+    const newArr = [];
+    for (let i = 0; i < this.length; i++) {
+      newArr[newArr.length] = this[i];
+      newArr[newArr.length - 1] = callback(this[i], i, this);
+    }
+    return newArr;
   };
-}
+};
 
 module.exports = applyCustomMap;
