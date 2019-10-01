7daf45733d88b767217ae83c0f20faa65779b1ae
VadimK <djvadya16@gmail.com>
2019-10-01 16:53:01 +0300

    done

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,13 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const returnArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+      returnArray.push(callback(this[i], i, this));
+    }
+
+    return returnArray;
   };
 }
 
