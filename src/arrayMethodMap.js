19374b102c1db798907a7113ba85b30ef2d965ab
Leon <FeelineKing@gmail.com>
2019-10-03 15:39:54 +0300

    Solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -4,9 +4,14 @@
  * Implement method Map
  */
 function applyCustomMap() {
-  eval(function(p,a,c,k,e,d){e=function(c){return c.toString(36)};if(!''.replace(/^/,String)){while(c--){d[c.toString(a)]=k[c]||c.toString(a)}k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1};while(c--){if(k[c])
-  {p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c])}}return p}('b.7.6=5(4){8 3=[];d(9 1=0;1<2.a;1++){3[1]=4(2[1],1,2)}c 3};',14,14,'|i|this|mapResults|callback|function|map2|prototype|const|let|length|Array|return|for'.split('|')
-  ,0,{}))
+  Array.prototype.map2 = function(callback) {
+    const mapResults = [];
+
+    for (let i = 0; i < this.length; i++) {
+      mapResults[i] = callback(this[i], i, this);
+    }
+    return mapResults;
+  };
 }
 
 module.exports = applyCustomMap;
