a8e3a8110396ff20484c0bde986f7cb5e8d27274
Senya Volkov <formobileprima@gmail.com>
2020-01-27 17:30:51 +0200

    solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -3,9 +3,16 @@
 /**
  * Implement method Map
  */
+
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      result.push(callback(this[i], i, this));
+    }
+
+    return result;
   };
 }
 
