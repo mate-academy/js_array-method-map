7100278dae1bee696e706303cfbe9a2391911246
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-27 20:36:05 +0200

    add solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,13 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const mappedArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      mappedArr.push(callback(this[i], i, this));
+    }
+
+    return mappedArr;
   };
 }
 
