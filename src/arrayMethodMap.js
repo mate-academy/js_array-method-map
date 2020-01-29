8d7158af1f048f2108714f43ef82a71e12ec008e
SahaPylypyuk <sashapulupyk@gmail.com>
2020-01-29 13:34:40 +0200

    Task implemented

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,8 +5,19 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const mapedArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+      mapedArray[i] = callback(this[i], i, this);
+    }
+
+    return mapedArray;
   };
 }
 
+applyCustomMap();
+
+const source = [1, 2, 3, 4, 5, 6];
+
+source.map2(x => x * 2);
 module.exports = applyCustomMap;
