c53a913c5fa844beb23abe2b6c65c5cffacff581
Nastia-rosy <nastiiarosy@gmail.com>
2020-04-05 17:04:11 +0300

    Solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -6,6 +6,13 @@
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
     // write code here
+    const result = [];
+
+    for (let i = 0; i < this.length; i++) {
+      result.push(callback(this[i], i, this));
+    }
+
+    return result;
   };
 }
 
