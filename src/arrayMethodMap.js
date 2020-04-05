d6d20bea89218e22d2c13a80b185cc6aa4c80516
kate <kate@test.com>
2020-04-05 18:34:51 +0300

    solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -6,6 +6,14 @@
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
     // write code here
+    const arr = this;
+    const res = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      res.push(callback(arr[i], i, arr));
+    }
+
+    return res;
   };
 }
 
