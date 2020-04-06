e825374e27f00cf1a768bf048a520235dbc1eb71
nvaho <iinazarovc@gmail.com>
2020-04-06 14:54:56 +0300

    hmm

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -6,6 +6,15 @@
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
     // write code here
+    const resArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      const resItem = callback(this[i], i, this);
+
+      resArr.push(resItem);
+    }
+
+    return resArr;
   };
 }
 
