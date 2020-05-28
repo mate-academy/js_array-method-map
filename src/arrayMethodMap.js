ca792e9cda47f314b9a33733d20160102699aea6
Liliia <chuiko1112@gmail.com>
2020-05-28 22:03:10 +0300

    map

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,13 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const output = [];
+
+    for (let i = 0; i < this.length; i++) {
+      output.push(callback(this[i], i, this));
+    }
+
+    return output;
   };
 }
 
