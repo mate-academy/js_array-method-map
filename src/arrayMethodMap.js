acf7f3f2ea089d539f9b357fda4db9bcd2478b21
Andrew Nickyforov <nik_andrey@ukr.net>
2020-05-31 16:21:38 +0300

    solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,7 +5,13 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const changedItems = [];
+
+    for (let i = 0; i < this.length; i++) {
+      changedItems[i] = callback(this[i], i, this);
+    }
+
+    return changedItems;
   };
 }
 
