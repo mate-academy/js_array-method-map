8379d75b8ddd4cc503bb9ff51750dd97d8b0063a
VlaDinter <vladyslavdinter@gmail.com>
2020-05-31 03:01:04 +0300

    Solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -1,11 +1,14 @@
 'use strict';
 
-/**
- * Implement method Map
- */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const mappedArray = [];
+
+    for (let i = 0; i < this.length; i++) {
+      mappedArray.push(callback(this[i], i, this));
+    }
+
+    return mappedArray;
   };
 }
 
