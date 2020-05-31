ef651e1ca20f8972343fa9e20184ee7bf2e88382
Denys <gryshko_denys@gmail.com>
2020-05-31 20:58:48 +0300

    solution

--- src/arrayMethodMap.js
+++ src/arrayMethodMap.js
@@ -5,8 +5,17 @@
  */
 function applyCustomMap() {
   [].__proto__.map2 = function(callback) {
-    // write code here
+    const changed = [];
+
+    for (let i = 0; i < this.length; i++) {
+      changed.push(callback(this[i], i, this));
+    }
+
+    return changed;
   };
 }
+
+// applyCustomFilter();
+// console.log([0, 10, 20, 30].filter2(a => a === 10));
 
 module.exports = applyCustomMap;
